3c41fae07d0c1318b134823039a05c9d6a9f8736
Mukesh Kumar_5375 <mukesh.kumar_5375@nihilent.com>
2020-01-03 11:29:28 +0530

    changes in action

--- actions/vm_create.py
+++ actions/vm_create.py
@@ -311,3 +311,39 @@ def create_nic(network_client):
     )
     return async_nic_creation.result()
 
+def create_vm_parameters(nic_id, vm_reference):
+    """Create the VM parameters structure.
+    """
+    return {
+        'location': LOCATION,
+        'os_profile': {
+            'computer_name': VM_NAME,
+            'admin_username': USERNAME,
+            'admin_password': PASSWORD
+        },
+        'hardware_profile': {
+            'vm_size': 'Standard_DS1'
+        },
+        'storage_profile': {
+            'image_reference': {
+                'publisher': vm_reference['publisher'],
+                'offer': vm_reference['offer'],
+                'sku': vm_reference['sku'],
+                'version': vm_reference['version']
+            },
+            'os_disk': {
+                'name': OS_DISK_NAME,
+                'caching': 'None',
+                'create_option': 'fromImage',
+                'vhd': {
+                    'uri': 'https://{}.blob.core.windows.net/vhds/{}.vhd'.format(
+                        STORAGE_ACCOUNT_NAME, VM_NAME+haikunator.haikunate())
+                }
+            },
+        },
+        'network_profile': {
+            'network_interfaces': [{
+                'id': nic_id,
+            }]
+        },
+    }
